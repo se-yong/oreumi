fd8fd71bf2d0163b24d50086ef02e80587d2e6b2
parkseyong <parksae0428@gmail.com>
2023-07-12 15:42:57 +0900

    # 2023-07-12

---
+++ algoritm/bfs1.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+def bfs(graph, start):    
+    visited = set()
+    queue = deque([start])
+    
+    while queue:
+        node = queue.popleft()
+        if node not in visited:
+            visited.add(node)
+            print(node, end=' ')
+            
+            queue.extend(graph[node] - visited) # 집합 - 집합 => 차집합
+
+graph = {
+    'A': set(['B', 'C']),
+    'B': set(['A', 'D', 'E']),
+    'C': set(['A', 'F']),
+    'D': set(['B']),
+    'E': set(['B', 'F']),
+    'F': set(['C', 'E'])
+}
+
+bfs(graph, start='A')
