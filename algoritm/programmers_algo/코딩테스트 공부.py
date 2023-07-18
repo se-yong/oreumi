553a389d8d6ee2081621727fa7f1f8da0dfef737
parkseyong <parksae0428@gmail.com>
2023-07-18 17:51:04 +0900

    # 2023-07-18

--- algoritm/programmers_algo/코딩테스트 공부.py
+++ algoritm/programmers_algo/코딩테스트 공부.py
@@ -24,3 +24,29 @@ def solution(alp, cop, problems):
                     next_alp,next_cop = min(max_alp,i + alp_rwd), min(max_cop,j + cop_rwd)
                     dp[next_alp][next_cop] = min(dp[next_alp][next_cop],dp[i][j] + cost)
     return dp[-1][-1] 
+
+# 다른 답안 
+
+# import sys
+# def solution(alp, cop, problems):
+
+#     alp_max = max(problems, key=lambda x:x[0])[0]
+#     cop_max = max(problems, key=lambda x:x[1])[1]
+
+#     dp = [[sys.maxsize] * (cop_max + 1) for _ in range(alp_max + 1)]
+#     alp = min(alp, alp_max)
+#     cop = min(cop, cop_max)
+
+#     for i in range(alp, alp_max + 1):
+#         for j in range(cop, cop_max + 1):
+#             if i + 1 <= alp_max:
+#                 dp[i+1][j] = min(dp[i+1][j], dp[i][j] + 1)
+#             if j + 1 <= cop_max:
+#                 dp[i][j+1] = min(dp[i][j+1], dp[i][j] + 1)
+
+#             for alp_req, cop_req, alp_rwd, cop_rwd, cost in problems:
+#                 if i >= alp_req and j >= cop_req:
+#                     next_alp, next_cop = min(alp_max, i + alp_rwd), min(cop_max, j + cop_rwd)
+#                     dp[next_alp][next_cop] = min(dp[next_alp][next_cop], dp[i][j] + cost)
+
+#     return answer
